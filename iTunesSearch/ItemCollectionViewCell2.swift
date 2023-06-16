2871b239089dd3aaf218f406982c3c062996487f
David Granger <dpgranger8@gmail.com>
2023-06-16 08:02:37 -0600

    part 1 of project complete, collection view layout finished

---
+++ iTunesSearch/ItemCollectionViewCell2.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCollectionViewCell.swift
+//  iTunesSearch
+//
+//  Created by David Granger on 6/9/23.
+//
+
+import UIKit
+
+class ItemCollectionViewCell2: UICollectionViewCell, ItemDisplaying {
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+}
